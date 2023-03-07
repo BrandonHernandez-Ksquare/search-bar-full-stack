dc1c8e6d910761cd0eb2e2a345d454155a779edd
Brandon Hernandez <brandon.hernandez@itksquare.edu.mx>
2023-03-07 12:01:10 -0600

    Brandon's PR

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './App.module.css'
 import PreviewOutlet from '@/components/PreviewOutlet';
 import SearchBar from '@/components/SearchBar';
 
 function App(): JSX.Element {
+
+  const [searcValue, setSearchValue] = useState();
+  const [results, setResults] = useState();
+  // I was about to start with this but ran out of time
+
+
+
   return (
     <div className={styles.App}>
      <SearchBar />
--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
 // your server here!!
+const express = require('express');
+const videogames = (require('./mock-db/data.json'));
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+});
+
+app.get('/video-games', (req, res) => {
+    const { search } = req.query;
+
+    console.log("search ->", search);
+
+    if(!search){
+        res
+            .status(200)
+            .send({result: videogames.videogames})
+        }
+    
+    const filteredVideoGames = videogames.videogames.filter(({title, releaseYear, developer}) => 
+        title.toLowerCase().includes(search) 
+        || releaseYear.includes(search) 
+        || developer.toLowerCase().includes(search)
+        );
+    
+
+    res
+        .status(200)
+        .send({result: filteredVideoGames})
+  });
+  
+
+app.listen(3000, () => {
+  console.log('Server is on');
+});
